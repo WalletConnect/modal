22c8bee2e0ff1a8a59397ad4347d1c05aa8b7973
Gancho Radkov <ganchoradkov93@gmail.com>
2024-09-30 13:40:40 +0300

    fix: window check

--- packages/modal-core/src/utils/CoreUtil.ts
+++ packages/modal-core/src/utils/CoreUtil.ts
@@ -38,13 +38,13 @@ export const CoreUtil = {
 
   isTelegram() {
     return (
-      (typeof window !== 'undefined' &&
+      typeof window !== 'undefined' &&
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      (Boolean((window as any).TelegramWebviewProxy) ||
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        Boolean((window as any).TelegramWebviewProxy)) ||
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      Boolean((window as any).Telegram) ||
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      Boolean((window as any).TelegramWebviewProxyProto)
+        Boolean((window as any).Telegram) ||
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        Boolean((window as any).TelegramWebviewProxyProto))
     )
   },
 
