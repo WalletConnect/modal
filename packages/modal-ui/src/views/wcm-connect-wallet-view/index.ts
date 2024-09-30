0f73010617f0655cc1167f17ef7323ac32697429
Gancho Radkov <ganchoradkov93@gmail.com>
2024-09-30 13:27:39 +0300

    chore: reenables custom android deeplinks on non tg envs

--- packages/modal-ui/src/views/wcm-connect-wallet-view/index.ts
+++ packages/modal-ui/src/views/wcm-connect-wallet-view/index.ts
@@ -9,12 +9,10 @@ export class WcmConnectWalletView extends LitElement {
 
   // -- private ------------------------------------------------------ //
   private viewTemplate() {
-    // Disabled custom android deeplink handling because it doesn't work in telegram mini apps
-    /*
-     * If (CoreUtil.isAndroid()) {
-     *   return html`<wcm-android-wallet-selection></wcm-android-wallet-selection>`
-     * }
-     */
+    // Disabled custom android deeplink handling in telegram mini apps because it doesn't work
+    if (CoreUtil.isAndroid() && !CoreUtil.isTelegram()) {
+      return html`<wcm-android-wallet-selection></wcm-android-wallet-selection>`
+    }
 
     if (CoreUtil.isMobile()) {
       return html`<wcm-mobile-wallet-selection></wcm-mobile-wallet-selection>`
